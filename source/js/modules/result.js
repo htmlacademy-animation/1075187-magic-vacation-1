f92a79699451064933c99ca50deabdaed614be29
grigorievk <grigorievka@gmail.com>
2021-03-06 17:52:48 +0300

    Add js function for path drawing. Add function for chars bouncing.

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -14,7 +14,10 @@ export default () => {
         });
         targetEl[0].classList.add(`screen--show`);
         targetEl[0].classList.remove(`screen--hidden`);
-        document.getElementById(`${target}Title`).beginElement();
+
+        const animateTitle = `${target}Title`;
+        setTitleAnimation(animateTitle);
+
       });
     }
 
@@ -31,4 +34,34 @@ export default () => {
     }
   }
 
+
+  function setTitleAnimation(elemId) {
+    const elem = document.getElementById(elemId);
+    const pathList = [...elem.querySelectorAll(`path`)];
+
+    pathList.forEach((path, index) => {
+      const pathLength = path.getTotalLength() / 2;
+      const pathAnimate = path.querySelector(`animate`);
+
+      path.setAttribute(`stroke-dasharray`, `0 ${pathLength}`);
+      pathAnimate.setAttribute(`to`, `${pathLength} 0`);
+
+      if (elemId === 'result3Title') {
+        setBounceAnimation(path, index);
+
+        const delay = pathAnimate.getAttribute('dur');
+        pathAnimate.setAttribute(`begin`, `${delay + (0.2 * index)}`);
+      }
+
+      pathAnimate.beginElement();
+    });
+  }
+
+  function setBounceAnimation(path, index) {
+    const pathAnimateTransform = path.querySelector(`animateTransform`);
+    const bounceSpeed = 1.3;
+
+    pathAnimateTransform.setAttribute(`keyTimes`, `0; ${0.05 + (0.05 * index)}; ${0.68 + (0.01 * index)}; ${0.85 + (0.01 * index)}; 1`);
+    pathAnimateTransform.setAttribute(`dur`, `${bounceSpeed - (0.1 * index)}s`);
+  }
 };
